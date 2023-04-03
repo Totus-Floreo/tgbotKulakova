d00fcf994ea97ebb6d8a5ae50bbceeac8ebecb7c
Timur Kulakov <kulakovcore@gmail.com>
2023-04-03 17:43:39 +0300

    token

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"os"
 
+	config "github.com/Totus-Floreo/tgbotKulakova/config"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
+	bot, err := tgbotapi.NewBotAPI(os.Getenv(config.TELEGRAM_APITOKEN))
 	if err != nil {
 		panic(err)
 	}
